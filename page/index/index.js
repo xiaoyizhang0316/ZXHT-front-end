ad4000aac4d3f147fe3676d681ca026c3c463eaa
DESKTOP-A3HVCL5\Dennis <zglker@gmail.com>
2018-04-19 18:11:02 +1000

    1

--- page/index/index.js
+++ page/index/index.js
@@ -292,10 +292,9 @@ Page({
 	onPullDownRefresh() {
 		let self = this
 		console.log('--------下拉刷新-------')
-		COM.load('Util').netUtil(COM.load('CON').GET_TARGETSHOP_PRODUCTS_URL + app.globalData.openId + "/" + app.globalData.targetShopId, "GET", "", function (shopProducts) {
+		COM.load('NetUtil').netUtil(COM.load('CON').GET_TARGETSHOP_PRODUCTS_URL + app.globalData.openId + "/" + app.globalData.targetShopId, "GET", "", function (shopProducts) {
 
-			if (shopProducts) {
-				
+			if (shopProducts) {				
 				wx.setStorageSync({
 					key: "shopProducts",
 					data: shopProducts,
